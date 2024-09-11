0cfbb0175674cc0c8878e0195e14e943acc73af6
Karthikeyan1028 <karthikidd03@gmail.com>
2024-09-11 22:35:22 +0530

    Hall-booking task has been completed

---
+++ readme.md
@@ -0,0 +1,7 @@
+Created a Hall Booking API with node and express.
+https://node-hall-booking-x2wm.onrender.com/hall-booking/get-all-room - To GET All Room Details.
+https://node-hall-booking-x2wm.onrender.com/hall-booking/create-room - To POST the Room Details in the API.
+https://node-hall-booking-x2wm.onrender.com/hall-booking/booking-room - To POST the booked Room Details in the API.
+https://node-hall-booking-x2wm.onrender.com/hall-booking/booked-data - To GET all rooms with Booked Details.
+https://node-hall-booking-x2wm.onrender.com/hall-booking/customer-data - To GET all Customer with Booked Details
+https://node-hall-booking-x2wm.onrender.com/hall-booking/bookedCount/:customer_name - To GET Customer Details with number of times the room booked.
--- src/controller/Home.js
+++ src/controller/Home.js
@@ -4,7 +4,7 @@ const homePage = async (req, res) => {
           <div style="padding:10px; background=skyblue"><h1 style = "text-align:center">Hall Booking API</h1>
           <ul><li><h3>https://node-hall-booking-x2wm.onrender.com/hall-booking/get-all-room - To GET All Room Details.</h3></li>
           <li><h3>https://node-hall-booking-x2wm.onrender.com/hall-booking/create-room - To POST the Room Details in the API.</h3></li>
-          <li><h3>https://hnode-hall-booking-x2wm.onrender.com/hall-booking/booking-room - To POST the booked Room Details in the API.</h3></li>
+          <li><h3>https://node-hall-booking-x2wm.onrender.com/hall-booking/booking-room - To POST the booked Room Details in the API.</h3></li>
           <li><h3>https://node-hall-booking-x2wm.onrender.com/hall-booking/booked-data - To GET all rooms with Booked Details.</h3></li>
           <li><h3>https://node-hall-booking-x2wm.onrender.com/hall-booking/customer-data - To GET all Customer with Booked Details</h3></li>
           <li><h3>https://node-hall-booking-x2wm.onrender.com/hall-booking/bookedCount/:customer_name - To GET Customer Details with number of times the room booked.</h3></li>
